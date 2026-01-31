7dd37bcccdca0c0427c028e1ca30d5fbd473079b
nemoeriksson <nemoeriksson2005@gmail.com>
2026-01-31 18:49:13 +0100

    Added keybinds from earlier

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+local map = vim.keymap.set
+
+vim.keymap.set("n", "<A-S-h>", "<C-w>h")
+vim.keymap.set("n", "<A-S-j>", "<C-w>j")
+vim.keymap.set("n", "<A-S-k>", "<C-w>k")
+vim.keymap.set("n", "<A-S-l>", "<C-w>l")
+
+vim.keymap.set("n", "<A-S-n>", "gT")
+vim.keymap.set("n", "<A-S-m>", "gt")
